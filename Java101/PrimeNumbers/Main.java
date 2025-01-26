e355d88ac00bbf395098288b819a1c0a41658767
Ali Mert Kaya <mert_k4433@hotmail.com>
2025-01-26 13:46:32 +0300

    Added PrimeNumbers

---
+++ Java101/PrimeNumbers/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100");
+        
+        for (int num = 2; num <= 100; num++) {
+            boolean isPrime = true;
+
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.print(num + " ");
+        }
+    }
+}
